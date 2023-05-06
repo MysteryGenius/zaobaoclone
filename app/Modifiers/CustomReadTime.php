5e23342fd6acab69c78771b284ecdf2b9af0481f
Tan Jian Zhen <tanjianzhen3000@gmail.com>
2023-05-07 04:58:02 +0800

    first commit

---
+++ app/Modifiers/CustomReadTime.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modifiers;
+
+use Statamic\Modifiers\Modifier;
+
+class CustomReadTime extends Modifier
+{
+    /**
+     * Modify a value.
+     *
+     * @param mixed  $value    The value to be modified
+     * @param array  $params   Any parameters used in the modifier
+     * @param array  $context  Contextual values
+     * @return mixed
+     */
+    public function index($value, $params, $context)
+    {
+        // if params are passed, use them as the words per minute
+        if (count($params)) $wordsPerMinute = $params[0];
+        else $wordsPerMinute = 200;
+
+        $words = strlen(strip_tags($value));
+        $time = $words / $wordsPerMinute;
+
+        if ($time < 1) return '少于1分钟'; // if it is less than a minute, return "少于1分钟"
+        return ceil($time) . '分钟'; // else return ceil($time) . '分钟';
+    }
+}
---
+++ content/collections/pages/home.md
@@ -0,0 +1,10 @@
+---
+id: home
+blueprint: pages
+title: Home
+template: home
+author: c6024dea-b1b4-4128-b294-ecb4e05c85f3
+updated_by: c6024dea-b1b4-4128-b294-ecb4e05c85f3
+updated_at: 1683314050
+---
+新加坡最棒的假新闻
